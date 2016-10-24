5caed1fa3b71941f3620efeeaeaf9a6903a7a4d9
milangahatraj <milangahatraj66@gmail.com>
2016-10-24 11:51:22 +0545

    Add files via upload

--- quadratic_equation.c
+++ quadratic_equation.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 	int main(){
-		float a,b,c,d,e,f;
+		float a,b,c,d,root;
 		printf("\nEnter the value of a:");
 		scanf("%f",&a);
 		printf("\nEnter the value of b:");
@@ -12,13 +12,18 @@
 		
 		printf("Discriminant:%f",d);
 		if(d>0){
-			e=(-b+pow(d,0.5))/2*a;
-			printf("\nFirst root is:%f",e);
-			f=(-b-pow(d,0.5))/2*a;
-			printf("\nsecond root is:%f",f);
+			root=(-b+pow(d,0.5))/2*a;
+			printf("\nFirst root is:%f",root);
+			root=(-b-pow(d,0.5))/2*a;
+			printf("\nSecond root is:%f",root);
 		}
 		else{
 			printf("\nInvalid");
+			
+			
+			
+			
+			
 		}
 	}
 	
