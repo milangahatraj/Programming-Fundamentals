5e356f3d49dfca4f52e5675212e7aecfea7c8646
milangahatraj <milangahatraj66@gmail.com>
2016-10-24 09:55:34 +0545

    Add files via upload

--- simple_calculator.c
+++ simple_calculator.c
@@ -10,49 +10,60 @@ Date:2016-10-23
 */
 #include<stdio.h>
 //#include<conio.h>
+#include<stdlib.h>
 int main(){
 	int a,b,result;
-	char o;
-	printf("\nSelect an operator(+,-,*,/,%%)");
-	scanf("%c",&o);
-	printf("\nFirst Number:");
+	int o;
+	m:
+	printf("\n\t*SIMPLE CALCULATOR*");
+	printf("\n\t*Select 1 for Addition.\t*");
+	printf("\n\t*Select 2 for Subtraction.\t*");
+	printf("\n\t*Select 3 for Multiplication.\t*");
+	printf("\n\t*Select 4 for Division.\t");
+	
+	printf("\n\tEnter selective number:");
+	scanf("%d",&o);
+	printf("\n\tFirst Number:");
 	scanf("%d",&a);
 	
-	printf("\nSecond Number:");
+	printf("\n\tSecond Number:");
 	scanf("%d",&b);
 	
 	//o=getchar();
 	
 	
 	switch(o){
-		case'+':
+		case'+': case'1':
 			result=a+b;
-				printf("The sum of two numbner is:%d",result);
+				printf("\n\tThe sum of two numbner is:%d",result);
 				break;
 				
-		case'-':
+		case'-': case'2':
 			result=a-b;
-			printf("The difference of two numbner is:%d",result);
+			printf("\n\tThe difference of two numbner is:%d",result);
 			
 				break;
 				
-		case'*':
+		case'*': case'3':
 			result=a*b;
-				printf("The multiplication of two numbner is:%d",result);
+				printf("\n\tThe multiplication of two numbner is:%d",result);
 				break;
 				
-		case'/':
+		case'/': case'4':
 			result=a/b;
-				printf("The division of two numbner is:%d",result);
+				printf("\n\tThe division of two numbner is:%d",result);
 				break;
 				
 		case'%':
 			result=a%b;
-				printf("The modulus of two numbner is:%d",result);
+				printf("\n\tThe modulus of two numbner is:%d",result);
 				break;
+		case 0:
+			exit (0);
 		default:
-			printf("\nChoose valid operator");
+			printf("\n\tChoose valid operator");
 		}
+		goto m;
 		
 		return 0;
 	}
