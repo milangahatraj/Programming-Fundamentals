59f49be710e17e6797ea111826ac4fe9801eef1c
milangahatraj <milangahatraj66@gmail.com>
2016-10-24 11:01:06 +0545

    Add files via upload

--- ternary_operator.c
+++ ternary_operator.c
@@ -3,21 +3,25 @@ Student Name:Milan Gahatraj
 subject:Programming Fundamental
 Roll no.:
 Lab no.:
-program:WAP to take two input from user and print largest and smallest numbers using terminary operator.
+program:WAP to take three input from user and print largest and smallest numbers using terminary operator.
 Date:2016-10-24
 */
 #include<stdio.h>
 //#include<conio.h>
 
 int main(){
-	int a,b,largest,smallest;
+	int a,b,c;
 	
 	printf("Enter first number:");
 	scanf("%d",&a);
 	printf("Enter second number:");
-	
 	scanf("%d",&b);
-	(a>b)?printf("%d is largest and %d is smaller",a,b):printf("%d is largest and %d is smaller",b,a);
+	printf("Enter third number:");
+	scanf("%d",&c);
+	
+	(a>=b)?(a>b)?printf("%d is greater",a):printf("%d is greater",c):
+	(b>=c)?printf("%d is greater",b):printf("%d is greater",c);
+		
 	return 0;
 	}
 	
