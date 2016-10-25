c4876379afefdd104ec0cf8ea16af02e3c8d0212
milangahatraj <milangahatraj66@gmail.com>
2016-10-25 10:46:37 +0545

    Add files via upload

--- sum_average_product.c
+++ sum_average_product.c
@@ -15,27 +15,24 @@ smallest,largest of these three numbers.*/
 		printf("The average of three numbers is:%d,%d&%d=%d\n",n1,n2,n3,average);
 		product=n1*n2*n3;
 		printf("The product of three numbers is:%d,%d&%d=%d\n",n1,n2,n3,product);
-		if(n1>=n2){
-			if(n2>=n3)
-			printf("%d is greatest",n1);
-			else
-			printf("%d id greatest",n3);
-			}
-		else{
-			if(n2>=n3)
-				printf("%d is greatest",n2);
-			else
-				printf("%d is greatest\n",n3);
-			}
-			(n1<=n2)?(n1<=n3?
-			printf("%d is smallest\n",n1):
-			printf("%d is smallest",n3)
-			):
-			(n2<=n3)?
-			printf("%d is smallest",n2):
-			printf("%d is smallest",n3);
+		(n1>=n2)?(n1>=n3?
+		printf("%d is greatest",n1):
+		printf("%d is greatest",n3)
+		):
+		(n2>=n3)?
+		printf("%d is greatest",n2):
+		printf("%d is greatest\n",n3);
 		
-			return 0;
+		
+		(n1<=n2)?(n1<=n3?
+		printf("%d is smallest\n",n1):
+		printf("%d is smallest",n3)
+		):
+		(n2<=n3)?
+		printf("%d is smallest",n2):
+		printf("%d is smallest",n3);
+		
+		return 0;
 		
 			
 	}
